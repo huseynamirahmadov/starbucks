94c7eaa459250ad5da5653de6907265f3da2bee5
huseynamirahmadov <huseynamirahmadov@gmail.com>
2023-09-29 23:41:09 +0400

    footer-top added

--- src/layout/Footer/index.jsx
+++ src/layout/Footer/index.jsx
@@ -1,8 +1,80 @@
 import React from 'react'
+import './Footer.css'
+import { Link } from 'react-router-dom'
+
+
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <div className="container">
+        <div className="footer-top">
+          <div className="footer-box">
+            <h3>About Us</h3>
+            <ul>
+              <li><Link to='#'>Our Company</Link></li>
+              <li><Link to='#'>Out Coffee</Link></li>
+              <li><Link to='#'>Stories and News</Link></li>
+              <li><Link to='#'>Starbucks Archive</Link></li>
+              <li><Link to='#'>Investor Relations</Link></li>
+              <li><Link to='#'>Customer Service</Link></li>
+              <li><Link to='#'>Contact Us</Link></li>
+            </ul>
+          </div>
+          <div className="footer-box">
+            <h3>Careers</h3>
+            <ul>
+              <li><Link to='#'>Culture and Values </Link></li>
+              <li><Link to='#'>Inclusion, Diversity and Equity</Link></li>
+              <li><Link to='#'>College Achivement Plan</Link></li>
+              <li><Link to='#'>Alumni Community</Link></li>
+              <li><Link to='#'>U.S. Careers</Link></li>
+              <li><Link to='#'>International Careers</Link></li>
+            </ul>
+          </div>
+          <div className="footer-box">
+            <h3>Social Impact</h3>
+            <ul>
+              <li><Link to='#'>People</Link></li>
+              <li><Link to='#'>Planet</Link></li>
+              <li><Link to='#'>Environmental and Social Impact Reporting</Link></li>
+            </ul>
+          </div>
+          <div className="footer-box">
+            <h3>For Business Partners</h3>
+            <ul>
+              <li><Link to='#'>Landlord Support Center</Link></li>
+              <li><Link to='#'>Suppliers</Link></li>
+              <li><Link to='#'>Corporate Gift Cards Sales</Link></li>
+              <li><Link to='#'>Office and Foodservice Coffee</Link></li>
+            </ul>
+          </div>
+          <div className="footer-box">
+            <h3>Order and Pick Up</h3>
+            <ul>
+              <li><Link to='#'>Order on the App</Link></li>
+              <li><Link to='#'>Order on the Web</Link></li>
+              <li><Link to='#'>Delivery</Link></li>
+              <li><Link to='#'>Order and Pick Up Options</Link></li>
+              <li><Link to='#'>Explore and Find Coffee for Home</Link></li>
+            </ul>
+          </div>
+        </div>
+        <div className="footer-line"></div>
+        <div className="footer-bottom">
+          <div className="footer-socials">
+            <ul>
+              <li><Link to='#'></Link></li>
+              <li><Link to='#'></Link></li>
+              <li><Link to='#'></Link></li>
+              <li><Link to='#'></Link></li>
+              <li><Link to='#'></Link></li>
+              <li><Link to='#'></Link></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </footer>
   )
 }
 
--- src/layout/Navbar/index.jsx
+++ src/layout/Navbar/index.jsx
@@ -7,41 +7,43 @@ import MainLogo from '../../components/MainLogo';
 const Navbar = () => {
     return (
         <>
-            <div className="container">
-                <div className="navbar">
-                    <div className="navbar-left-side">
-                        <MainLogo />
-                        <div className="navbar-links">
-                            <ul>
-                                <li>
-                                    <Link to='/menu'>MENU</Link>
-                                </li>
-                                <li>
-                                    <Link to='/rewards'>REWARDS</Link>
-                                </li>
-                                <li>
-                                    <Link to='/giftcards'>GIFT CARDS</Link>
-                                </li>
-                            </ul>
+            <div className="navbar-box-shadow">
+                <div className="container">
+                    <div className="navbar">
+                        <div className="navbar-left-side">
+                            <MainLogo />
+                            <div className="navbar-links">
+                                <ul>
+                                    <li>
+                                        <Link to='/menu'>MENU</Link>
+                                    </li>
+                                    <li>
+                                        <Link to='/rewards'>REWARDS</Link>
+                                    </li>
+                                    <li>
+                                        <Link to='/giftcards'>GIFT CARDS</Link>
+                                    </li>
+                                </ul>
+                            </div>
                         </div>
-                    </div>
-                    <div className="navbar-right-side">
-                        <div className="find-a-store">
-                            <Link to='/findastore'>
-                                <MdLocationOn />
-                                <span>Find a store</span>
-                            </Link>
-                        </div>
-                        <div className="sign">
-                            <div className="sign-in">
-                                <Link to='signin'>
-                                    Sign in
+                        <div className="navbar-right-side">
+                            <div className="find-a-store">
+                                <Link to='/findastore'>
+                                    <MdLocationOn />
+                                    <span>Find a store</span>
                                 </Link>
                             </div>
-                            <div className="join-now">
-                                <Link to='joinnow'>
-                                    Join now
-                                </Link>
+                            <div className="sign">
+                                <div className="sign-in">
+                                    <Link to='signin'>
+                                        Sign in
+                                    </Link>
+                                </div>
+                                <div className="join-now">
+                                    <Link to='joinnow'>
+                                        Join now
+                                    </Link>
+                                </div>
                             </div>
                         </div>
                     </div>
