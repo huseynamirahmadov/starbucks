de76aa01897284cb923bcc655a7ea313b0b11a32
huseynamirahmadov <huseynamirahmadov@gmail.com>
2023-11-07 18:34:18 +0400

    circle animation added

--- src/pages/Product/index.jsx
+++ src/pages/Product/index.jsx
@@ -4,12 +4,34 @@ import { useDispatch, useSelector } from 'react-redux'
 import { Link, useParams } from 'react-router-dom'
 import { fetchContent } from '../../redux/slice/contentSlice'
 import menuLoadingImg from '../../assets/images/menuloadingimg.png'
+import { CiLocationOn } from 'react-icons/ci'
+import selectIcon from '../../assets/images/select-icon.svg'
 
 const Product = () => {
   const dispatch = useDispatch()
   useEffect(() => {
     dispatch(fetchContent())
   }, [dispatch])
+
+
+
+  const handleClick = (e) => {
+    const element = e.currentTarget;
+    const circle = element.querySelector('.md-click-circle');
+    circle.classList.remove('md-click-animate');
+    if (!circle.clientHeight && !circle.clientWidth) {
+      const d = Math.max(element.offsetWidth, element.offsetHeight);
+      circle.style.height = d + 'px';
+      circle.style.width = d + 'px';
+    }
+    const x = e.pageX - element.getBoundingClientRect().left - circle.offsetWidth / 2;
+    const y = e.pageY - element.getBoundingClientRect().top - circle.offsetHeight / 2;
+    circle.style.top = y + 'px';
+    circle.style.left = x + 'px';
+    circle.classList.add('md-click-animate');
+  };
+
+
 
   const contents = useSelector(state => state.content.contents)
   const isLoading = useSelector(state => state.content.isLoading)
@@ -77,9 +99,57 @@ const Product = () => {
                 }))
               }
             </div>
+            <div className='product-location'>
+              <div className='product-location-icon'>
+                <CiLocationOn />
+              </div>
+              <div>Select a store to view availability</div>
+            </div>
           </div>
           <div className='product-inner-item-right-side'>
             <h3>What's included</h3>
+            <div className="select-list">
+              <div className='circle-animation'>
+                <div className='material-click material-on-me' onClick={handleClick}>
+                  <div className='animated-input'>
+                    <span>Milk</span>
+                    <div className='select-icon'>
+                      <img src={selectIcon} alt="" />
+                    </div>
+                    <select name="" id="">
+                      <option value="">Heavy Cream</option>
+                      <option value="">Vanilla Sweet Cream</option>
+                      <option value="">Nonfat Milk</option>
+                      <option value="">2% Milk</option>
+                      <option value="">Whole Milk</option>
+                      <option value="">Breve (Half & Half)</option>
+                      <option value="">Almond</option>
+                      <option value="">Coconut</option>
+                      <option value="">Oatmilk</option>
+                      <option value="">Soy</option>
+                    </select>
+                  </div>
+                  <span className='md-click-circle'></span>
+                </div>
+              </div>
+              <div className='circle-animation'>
+                <div className='material-click material-on-me' onClick={handleClick}>
+                  <div className='animated-input'>
+                    <span>Milk</span>
+                    <div className='select-icon'>
+                      <img src={selectIcon} alt="" />
+                    </div>
+                    <select name="" id="">
+                      <option value="">Extra Ice</option>
+                      <option value="">Light Ice</option>
+                      <option value="">No Ice</option>
+                      <option value="">Ice</option>
+                    </select>
+                  </div>
+                  <span className='md-click-circle'></span>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
